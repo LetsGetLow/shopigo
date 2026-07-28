7bd19da0e14e1a6a2fd5f2c9f0fb82936e02d3e5
Mathias Lüer <mathias.lueer@etailer.de>
2026-07-28 16:44:22 +0200

    add SQL script to create catalog_categories table with necessary fields

---
+++ migrations/catalog/001_create_categories.sql
@@ -0,0 +1,12 @@
+CREATE TABLE catalog_categories (
+    category_id UUID PRIMARY KEY,
+    parent_id UUID REFERENCES catalog_categories (category_id),
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by UUID NOT NULL,
+    updated_at TIMESTAMPTZ NULL,
+    updated_by UUID NULL,
+    deleted_at TIMESTAMPTZ NULL,
+    deleted_by UUID NULL
+);
